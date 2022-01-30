06b12691092cf9984b89d6f79a4a1302166de85f
Robert Scheidegger <robert_scheidegger@brown.edu>
2022-01-29 20:43:08 -0500

    clean up data scraping

--- server/src/models/Course.ts
+++ server/src/models/Course.ts
@@ -1,39 +1,39 @@
 import Mongoose from "mongoose";
 
 const CourseSchema = new Mongoose.Schema({
-    name: {
-        type: String,
-        required: true   
-    },
-    code: {
-        type: String,
-        required: true
-    },
-    description: {
-        type: String,
-        required: false
-    },
-    prerequisites: {
-        type: [String],
-        required: false
-    },
-    corequisites: {
-        type: [String],
-        required: false
-    },
-    department: {
-        type: String,
-        required: true
-    }
+  name: {
+    type: String,
+    required: true,
+  },
+  code: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: false,
+  },
+  prerequisites: {
+    type: [String],
+    required: false,
+  },
+  corequisites: {
+    type: [String],
+    required: false,
+  },
+  department: {
+    type: String,
+    required: true,
+  },
 });
 
 export interface ICourse {
-    name: string;
-    code: string;
-    description: string;
-    prerequisites: string[];
-    corequisites: string[];
-    department: string;
+  name: string;
+  code: string;
+  description: string;
+  prerequisites: string[];
+  corequisites: string[];
+  department: string;
 }
 
-export const Course = Mongoose.model('Courses', CourseSchema);
+export const Course = Mongoose.model("Courses", CourseSchema);
--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -1,35 +1,41 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    semesters: [{
-        courses: [{
-            course: {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: 'Courses'
-            },
-        }],
-        completed: Boolean
-    }],
-    degrees: [{
-        degree: {
+  name: String,
+  email: String,
+  semesters: [
+    {
+      courses: [
+        {
+          course: {
             type: mongoose.Schema.Types.ObjectId,
-            ref: 'Degrees'
+            ref: "Courses",
+          },
         },
-        satisfications: Object // JSON Object with value of Requirement
-    }]
+      ],
+      completed: Boolean,
+    },
+  ],
+  degrees: [
+    {
+      degree: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Degrees",
+      },
+      satisfications: Object, // JSON Object with value of Requirement
+    },
+  ],
 });
 
 export interface Semester {
-    courses: mongoose.Types.ObjectId[];
-    completed: boolean;
+  courses: mongoose.Types.ObjectId[];
+  completed: boolean;
 }
 
 export interface IUser extends Document {
-    name: string;
-    email: string;
-    semesters: Semester
+  name: string;
+  email: string;
+  semesters: Semester;
 }
 
-export const User = mongoose.model('Users', UserSchema);
+export const User = mongoose.model("Users", UserSchema);
